65ffd77736200bbc9b83bc0d4e0ed2ab11b8bfd2
kilauakhyaari <kilauakhyaari6@gmail.com>
2023-09-11 18:31:00 +0700

    Modified html file

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'app': 'My Wardrobe',
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
+        'name': 'Kilau Nisrina Akhyaari',
+        'class': 'PBP E'
     }
 
     return render(request, "main.html", context)
