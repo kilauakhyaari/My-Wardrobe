c6174a31ade41d01a7478c9a117a7b3f05ab157f
kilauakhyaari <kilauakhyaari6@gmail.com>
2023-09-17 16:06:35 +0700

    progress tugas 3

--- MyWardrobe/settings.py
+++ MyWardrobe/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MyWardrobe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        
     },
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from main.forms import ProductForm
+from django.urls import reverse
+from django.core import serializers
+from main.models import Item
 
 def show_main(request):
+    items = Item.objects.all()
+
     context = {
         'app': 'My Wardrobe',
         'name': 'Kilau Nisrina Akhyaari',
-        'class': 'PBP E'
+        'class': 'PBP E',
+        'items': items
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
